4d306b9773681e7dbab0184a90e8437191146f10
UrosNerandzic <UrosNerandzic@users.noreply.github.com>
2024-05-20 17:47:02 +0000

    deploy: afaef51d797b225beaee545980049e28bf6d1749

---
+++ assets/Logo-21294db7.js
@@ -0,0 +1,1 @@
+import{j as o,L as s}from"./index-cc129a12.js";const l="_logo_7b2ls_1",t={logo:l};function g(){return o.jsx(s,{to:"/world-wise",children:o.jsx("img",{src:"./logo.png",alt:"WorldWise logo",className:t.logo})})}export{g as L};
